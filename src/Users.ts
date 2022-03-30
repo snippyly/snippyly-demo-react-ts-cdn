a96ddf8b1711bd4b6130717ad29b63cbce4d6c82
vivekk-snippyly <vivek@snippyly.com>
2022-03-31 00:31:38 +0530

    Updated snippyly to 1.0.23.

--- src/Users.ts
+++ src/Users.ts
@@ -2,7 +2,7 @@ export const Users = [
     {
         userId: '1',
         name: 'James Smith',
-        photoUrl: 'https://snippyly.com/assets/avatars/avatar-panda.svg',
+        photoUrl: '',
         email: 'james.smith@gmail.com',
         plan: 'free',
         groupId: ''
@@ -18,16 +18,8 @@ export const Users = [
     {
         userId: '3',
         name: 'Sarah Wilson',
-        photoUrl: 'https://snippyly.com/assets/avatars/avatar-monkey.svg',
-        email: 'sarah.wilson@gmail.com',
-        plan: 'trial',
-        groupId: ''
-    },
-    {
-        userId: '4',
-        name: 'James Bond',
         photoUrl: '',
-        email: 'james.bond@gmail.com',
+        email: 'sarah.wilson@gmail.com',
         plan: 'trial',
         groupId: ''
     }
