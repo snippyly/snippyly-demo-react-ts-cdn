96d0e02acf86a4b2be6e97390168d17938b7a235
vivekk-snippyly <vivek@snippyly.com>
2022-06-29 18:52:08 +0530

    Updated snippyly to 1.0.41.

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -41,6 +41,9 @@ function Toolbar({ onMenuSelect }: { onMenuSelect: Function }) {
     }
 
     const signOut = () => {
+        if (client) {
+            client.signOutUser();
+        }
         localStorage.removeItem('user');
         window.location.reload();
     }
