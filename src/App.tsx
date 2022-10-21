79f03c30393ef0860a61b609df1463c6b10ca515
vivek <vivek@snippyly.com>
2022-10-22 03:09:48 +0530

    Updated snippyly to 1.0.54.

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,7 @@ function App() {
         <snippyly-cursor></snippyly-cursor>
         <snippyly-comments></snippyly-comments>
         <snippyly-comments-sidebar></snippyly-comments-sidebar>
-        <snippyly-comment-tool>
-          <div className='add-comment-btn'>
-            <img src='https://cdn-icons-png.flaticon.com/512/727/727570.png' alt='Add comment' />
-          </div>
-        </snippyly-comment-tool>
+        <snippyly-comment-tool></snippyly-comment-tool>
         <Toolbar onMenuSelect={(menu: any) => setSelectedMenu(menu)} />
         <Tabs selectedMenu={selectedMenu} />
       </div>
